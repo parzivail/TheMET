19178be2166a3bfcf6995423bbc58417ade49f60
parzivail <colby.newman2000@gmail.com>
2021-12-09 17:10:09 -0500

    invert year similarity calculation to correspond to edge cost instead of similarity factor

--- MuseumObject.h
+++ MuseumObject.h
@@ -116,7 +116,7 @@ struct MuseumObjectDateComparator
     inline float operator()(const MuseumObject &a, const MuseumObject &b)
     {
         // determine similarity between two years, similarity drops in half after 15 years
-        return 1 / (std::pow(std::fabs(a.date - b.date) / 15, 3) + 1);
+        return std::pow(std::fabs(a.date - b.date) / 15, 3);
     }
 };
 
